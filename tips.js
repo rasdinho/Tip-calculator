dd1c2c17ccc5ca004848f66941caf705fa201d27
rasdinho <ras.dinho7@gmail.com>
2020-12-23 19:43:09 -0500

    add-js-css-files

---
+++ tips.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function (event){ 
+    console.log("DOM fully loaded and parsed");
+                            
+                       
+                         
+  })
+  //this function gets the amount from the user
+  function getAmount(){
+    
+  }
